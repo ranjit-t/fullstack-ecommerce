29a1177ec76e4da6a6027396138db03b7f144913
Ranjith <amailtoranjith@gmail.com>
2023-06-27 17:11:49 +0200

    orders in profile page

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -27,13 +27,27 @@ export default function Profile({ curUser }) {
   }, [curUser]);
 
   return (
-    <div>
-      <p>Profile</p>
+    <div className="pb-16">
+      <p className="text-xl font-bold mt-4 ">Orders</p>
       <div>
         {orders.map((order) => {
           return (
-            <div>
-              <p>num of items = {order.orderItems.length}</p>
+            <div className="flex flex-col justify-center items-center border border-gray-300 m-4 w-screen">
+              {/* <p>num of items = {order.orderItems.length}</p> */}
+              {order.orderItems.map((item) => {
+                return (
+                  <div className="flex justify-between items-between w-[80%]">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      width="80px"
+                      className="m-2 max-h-[40px] object-cover"
+                    />
+                    <p>{item.name.slice(0, 20)}...</p>
+                    <p>{item.price} euros</p>
+                  </div>
+                );
+              })}
             </div>
           );
         })}
